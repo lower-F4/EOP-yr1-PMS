757bf4a109953402efd9fd2b91018760816d673b
lower-f4 <fareesafandi05@gmail.com>
2024-12-30 18:48:55 +0800

    Add main java file

---
+++ PMSMain.java
@@ -0,0 +1,26 @@
+/*
+ * Patient Management System 
+ * by Group 5, BICS 1301, Year 1 Sem 1
+*/
+
+
+public class PMSMain {
+    public static void main(String[] args) {
+        //TODO main:
+        /*
+        - While loop that include a user input to exit/stay in program.
+        - The array which represent the table column.
+        - 
+        */              
+    }
+    //TODO query():
+    /*
+    -  to ask for user the option of each operation methods.
+    */
+
+
+    //TODO view():
+    /*
+    - to display the detail of each a certain/ whole patient table.
+    */
+}
