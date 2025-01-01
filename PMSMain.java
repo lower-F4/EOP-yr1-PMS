05f5343001bb35b867818b11fcc5f633a7a25ff6
Mansaaaa <aiman.salehudin@live.iium.edu.my>
2025-01-01 12:05:21 +0800

    VIEW PMSMain.java
    dah letak method view, bleum 100% lengkap tpi overall idea dapat

--- PMSMain.java
+++ PMSMain.java
@@ -2,7 +2,7 @@
  * Patient Management System 
  * by Group 5, BICS 1301, Year 1 Sem 1
 */
-
+import java.util.Scanner;
 
 public class PMSMain {
     public static void main(String[] args) {
@@ -29,4 +29,24 @@ public class PMSMain {
     /*
     - to display the detail of each a certain/ whole patient table.
     */
+    public static void view(int[] ptid, String[] ptname, String[] disease, int[] docid){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please key-in which patient's information that you wish to view: \n");
+
+        for (int i = 0 ; i<ptid.length ; i++){
+            System.out.println( (i+1) + ". " + ptname[i]);
+        }
+        System.out.println( (ptid.length+1) + ". " + "VIEW ALL PATIENTS");
+        int viewname = input.nextInt();
+
+        int loopname = viewname - 1;
+
+        System.out.println("Patient ID    Patient Name    Disease    Doctor ID");
+        System.out.println("---------------------------------------------------");
+        
+        if (viewname > 0 && viewname != (ptid.length+1))
+            System.out.printf("%-10f    %-11s    %-7s    %-9f\n", ptid[loopname],ptname[loopname],disease[loopname],docid[loopname]);
+        else if (viewname == (ptid.length+1))
+            for (int i = 0 ; i < ptid.length ; i++)
+                    System.out.printf("%-10f    %-11s    %-7s    %-9f\n", ptid[i],ptname[i],disease[i],docid[i]);
 }
